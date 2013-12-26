5ff0b3868700d9ce820b1cea4e61aaf71f9b10b6
Jianzheng Zhou <water.zhou2011@gmail.com>
2013-12-26 18:43:58 +0800

    define CONFIG_SKIP_LOWLEVEL_INIT to avoid double init.
    In fact it has been done by loader.bin
    
    Signed-off-by: Jianzheng Zhou <water.zhou2011@gmail.com>

--- u-boot-1.1.6/include/configs/at91rm9200dk.h
+++ u-boot-1.1.6/include/configs/at91rm9200dk.h
@@ -41,7 +41,7 @@
 #define CONFIG_CMDLINE_TAG	1	/* enable passing of ATAGs	*/
 #define CONFIG_SETUP_MEMORY_TAGS 1
 #define CONFIG_INITRD_TAG	1
-
+#define CONFIG_SKIP_LOWLEVEL_INIT
 #ifndef CONFIG_SKIP_LOWLEVEL_INIT
 #define CFG_USE_MAIN_OSCILLATOR		1
 /* flash */
